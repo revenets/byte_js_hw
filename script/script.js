0fff8721130862421ceb7de310c5372923aec41b
revenets <89215185+revenets@users.noreply.github.com>
2021-12-01 11:46:46 +0200

    add new variables

--- script/script.js
+++ script/script.js
@@ -2,7 +2,11 @@ let userName = "Dima";
 let userSurname = "Revenets";
 let userAge = 28;
 
-
+let newUser;
+let shoppingList;
+let isUserOnline = true;
+let userSalary;
+let cursorCoords;
 
 const greetingWorld = "Hello, ";
 console.log(greetingWorld + userName + "!");
