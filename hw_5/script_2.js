04312607c503af74d0360ffe7b26bf563f07182c
Revenets <revenets@gmail.com>
2021-12-10 11:44:25 +0200

    correcting task 2

--- hw_5/script_2.js
+++ hw_5/script_2.js
@@ -1,8 +1,38 @@
-let name;
-let attempt = 3;
-do {
-    name = prompt("Enter your name");
-    attempt--;
-    console.log(`name`, name);
-    !name ? alert(`${attempt} attempts left`) : alert("Welcome");
-} while (!name && attempt > 0);
+const LOGIN = "admin";
+const PASSWORD = "1q2w3e";
+
+const authorize = () => {
+    let userLogin;
+    let userPassword;
+    let isAuthSuccess = false;
+
+    for(let i = 2; i >= 0; i--) {
+        userLogin = prompt("Enter login: ");
+        if (!userLogin) {
+            alert("This field is required");
+            i = 2;
+            continue;
+        }
+
+        userPassword = prompt("Enter password: ");
+        if (!userPassword) {
+            alert("This field is required");
+            i = 2;
+            continue;
+        }
+
+        if (userLogin === LOGIN && userPassword === PASSWORD) {
+            isAuthSuccess = true;
+            alert("Welcome!");
+            break;
+        } else {
+            alert(`Incorrect input! ${i} attempts left`);
+        }
+    }
+
+    if(!isAuthSuccess) {
+        alert("Access denied!");
+    }
+}
+
+authorize();
